e564bb35143ff58634e9a62822b68f79c707fbde
Santi-Gilis <rubentxu74@gmail.com>
2016-03-24 12:10:33 +0100

    modules

---
+++ include/LogicBrick.h
@@ -0,0 +1,8 @@
+//
+// Created by santi on 24/03/16.
+//
+
+#ifndef ILARGIA_LOGICBRICK_H
+#define ILARGIA_LOGICBRICK_H
+
+#endif //ILARGIA_LOGICBRICK_H
---
+++ include/sensors/Sensor.h
@@ -0,0 +1,8 @@
+//
+// Created by santi on 24/03/16.
+//
+
+#ifndef ILARGIA_SENSOR_H
+#define ILARGIA_SENSOR_H
+
+#endif //ILARGIA_SENSOR_H
